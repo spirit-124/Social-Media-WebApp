a40a2cbabfec44d160f7eac04db8b366ebdf6bde
spirit-124 <bajrangofficial580@gmail.com>
2023-09-13 22:58:26 +0530

    Minor Changes

--- server/controllers/AuthControllers.js
+++ server/controllers/AuthControllers.js
@@ -26,16 +26,7 @@ export const registerUser = async (req, res) => {
     // IF NEW USER
 
     const user = await newUser.save();
-    // const token = jwt.sign(
-    //   {
-    //     username: user.username,
-    //     id: user._id,
-    //   },
-    //   process.env.JWT_KEY,
-    //   {
-    //     expiresIn: "1h",
-    //   }
-    // );
+
     res.status(200).json({ user });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -52,14 +43,8 @@ export const loginUser = async (req, res) => {
       if (!validity) {
         res.status(401).json("Wrong password");
       } else {
-        // const token = jwt.sign(
-        //   { username: user.username, id: user._id },
-        //   process.env.JWT_KEY,
-        //   { expiresIn: "1h" }
-        // );
         res.status(200).json(user);
       }
-      // res.status(200).json({ user, token });
     } else {
       res.status(404).json("User not found");
     }
